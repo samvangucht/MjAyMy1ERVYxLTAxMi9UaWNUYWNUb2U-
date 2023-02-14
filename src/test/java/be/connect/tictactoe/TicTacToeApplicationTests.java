e8df0aa1217c46eec73b4fcfcb385a7c34422555
samvangucht <samvangucht@hotmail.com>
2023-02-14 20:44:24 +0100

    initial commit -> setup

---
+++ src/test/java/be/connect/tictactoe/TicTacToeApplicationTests.java
@@ -0,0 +1,13 @@
+package be.connect.tictactoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
